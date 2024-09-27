f125f2eb938c8cb2fccbbcb72c03f2eacc3e89e1
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-09-27 17:10:09 +0700

    Update Universal script Non Limited.lua

--- Universal script Non Limited.lua
+++ Universal script Non Limited.lua
@@ -4,4 +4,6 @@ elseif game.PlaceId == 7655745946 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/Dunking%20Simulator.lua", true))()
 elseif game.PlaceId == 5306359293 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/island%20of%20move.lua", true))()
+elseif game.PlaceId == 6901029464 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/Mansion%20of%20wonder.lua", true))() 
 end
