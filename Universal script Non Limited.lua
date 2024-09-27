912c66c08ae33c256a68021e92cba7bfcb9068a4
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-09-27 17:18:27 +0700

    Update Universal script Non Limited.lua

--- Universal script Non Limited.lua
+++ Universal script Non Limited.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 7603178367 then
+if game.PlaceId == 7603178367 or game.PlaceId == 9298854137 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/Chipotle%20Burrito%20Builder.lua", true))()
 elseif game.PlaceId == 7655745946 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/Dunking%20Simulator.lua", true))()
