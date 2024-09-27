549545ba2d691b827a13e7cf44f6fe6f57495b4b
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-09-27 19:16:30 +0700

    Update Universal script Non Limited.lua

--- Universal script Non Limited.lua
+++ Universal script Non Limited.lua
@@ -5,13 +5,13 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limite
 elseif game.PlaceId == 5306359293 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/island%20of%20move.lua", true))()
 elseif game.PlaceId == 6901029464 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/Mansion%20of%20wonder.lua", true))() 
-elseif game.Place == 8526353932 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/Mansion%20of%20wonder.lua", true))()
+elseif game.PlaceId == 8526353932 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/McLaren%20F1%20Racing%20Experience.lua", true))()
 elseif game.PlaceId == 6225076142 or game.PlaceId == 6403132498 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/8th%20Annual%20Bloxy%20Awards.lua", true))()
 elseif game.PlaceId == 10146432319 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/Guacathon.lua", true))()
-elseif game.PlaceId == 10956766913 then 
+elseif game.PlaceId == 10956766913 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/SHEIN%20x%20Klarna%20Wonderland%20New%20Quest.lua", true))()
 end
