2b27d681ab423e3d17ca01f8d7e7bb7d91fc082c
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-09-27 16:56:45 +0700

    Update Universal script Non Limited.lua

--- Universal script Non Limited.lua
+++ Universal script Non Limited.lua
@@ -1,1 +1,3 @@
 if game.PlaceId == 7603178367 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/Chipotle%20Burrito%20Builder.lua", true))()
+end
