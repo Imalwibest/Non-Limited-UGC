aa0a7bad57d1e56317c582aa3484f5dcf9c46739
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-09-27 21:54:41 +0700

    Create Roblox Innovation Awards Voting Hub.lua

---
+++ Roblox Innovation Awards Voting Hub.lua
@@ -0,0 +1,25 @@
+    local tweenInfo = TweenInfo.new(2)
+    local goal = {CFrame = CFrame.new(-3, 23, -116)}
+    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
+    tween:Play()
+wait(5) 
+    local tweenInfo = TweenInfo.new(2)
+    local goal = {CFrame = CFrame.new(155, 45, -51)}
+    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
+    tween:Play()
+
+wait(5) 
+    local tweenInfo = TweenInfo.new(2)
+    local goal = {CFrame = CFrame.new(-4, 65, -199)}
+    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
+    tween:Play()
+wait(3) 
+game.Players.LocalPlayer.Character:PivotTo(CFrame.new(Vector3.new(-343, 6, 357)))
+game.Players.LocalPlayer.Character:PivotTo(CFrame.new(Vector3.new(-343, 6, 357)))
+wait(1) 
+    local tweenInfo = TweenInfo.new(2)
+    local goal = {CFrame = CFrame.new(-162, 39, -148)}
+    local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, tweenInfo, goal)
+    tween:Play()
+wait(3) 
+game.Players.LocalPlayer.Character:PivotTo(CFrame.new(Vector3.new(95, 168, -151)))
