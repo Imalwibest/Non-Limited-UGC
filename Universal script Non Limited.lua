b38391e03542dad5e3660a403465d0f960e4b939
Imalwibest <140151091+Imalwibest@users.noreply.github.com>
2024-09-27 18:06:31 +0700

    Update Universal script Non Limited.lua

--- Universal script Non Limited.lua
+++ Universal script Non Limited.lua
@@ -8,4 +8,6 @@ elseif game.PlaceId == 6901029464 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/Mansion%20of%20wonder.lua", true))() 
 elseif game.Place == 8526353932 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/McLaren%20F1%20Racing%20Experience.lua", true))()
+elseif game.PlaceId == 6225076142 or game.PlaceId == 6403132498 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Imalwibest/Non-Limited-UGC/refs/heads/main/8th%20Annual%20Bloxy%20Awards.lua", true))()
 end
